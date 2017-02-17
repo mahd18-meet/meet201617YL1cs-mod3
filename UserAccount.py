0c20d7dd6fff72989e1e0fbdd425ab884d82ba99
mahd18-meet <mqumseya@js-bethlehem.com>
2017-02-17 07:48:00 -0500

    Finished product

--- UserAccount.py
+++ UserAccount.py
@@ -3,17 +3,26 @@ class UserAccount :
     A class that models a user account for mega-social-media website, GuessMySecret
     """
 
-    def __init__(self, username, password, secret):
+    def __init__(self, my_username, my_password, my_secret):
         """
         Initialize the UserAccount object:
         :param username: user name (string)
         :param password: password to gain access to account (string)
         :param secret: user's secret, accessible only with password (string)
         """
-        self.username=username
+        self.username=my_username
+        self.secret = my_secret
+        self.password =my_password
+        
+        
         #Complete the __init__ method
 
     def print_secret(self,password_attempt):
+        if self.password == password_attempt:
+            print (self.secret)
+        else:
+            print ("Invalid")
+        
         """
         Checks user input, password_attempt - if this matches the user's
         password, then the user's secret is printed to the screen.
